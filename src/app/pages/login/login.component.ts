892d6bc1322550cf96f29c497a970d6ddbd47510
ajurado301 <ajurado301@gmail.com>
2022-03-16 16:13:10 +0100

    Creado modal solicitud password

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -12,4 +12,8 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // TODO: Provisionalpara abrir modal
+  modalPass(veloModalPass: HTMLElement, visible: boolean) {
+    veloModalPass.style.display = (visible) ? 'flex' : 'none';
+  }
 }
