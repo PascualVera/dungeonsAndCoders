f0b7218d4a193bda6f7873a2ecba324c5b4b8756
PascualVera <pascualvera.994@gmail.com>
2022-03-18 11:02:35 +0100

    heroku url actualizada

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express()
 
 app.use(express.static(__dirname+ 'dist/dungeons-and-coder'))
 app.get('/*', function(req,res){
-  res.sendFile('index.html', {root: __dirname + '/dist/dungeons-and-coder/'})
+  res.sendFile('index.html', {root: __dirname + '/dist/dungeons-and-coder'})
 })
 app.listen(process.env.PORT || 8080)
