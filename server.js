1effd97aee13f2ab821113b38d8d7fd938fff789
PascualVera <pascualvera.994@gmail.com>
2022-03-18 10:41:52 +0100

    url server updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.use(express.static(__dirname+ 'dist/dungeons-and-coder'))
-app.get('/*', function(re,res){
-  res.sendFile('index.html', {root: __dirname + 'dungeons-and-coder'})
+app.get('/*', function(req,res){
+  res.sendFile('index.html', {root: __dirname + '/dist/dungeons-and-coder/'})
 })
 app.listen(process.env.PORT || 8080)
